fce376d17c4c62861f533f959885a1ad75be288f
AbhinavRattan27 <83867719+AbhinavRattan27@users.noreply.github.com>
2022-02-27 13:54:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function lfthnd(){
+ document.getElementById("badluck").innerHTML="You Win!";
+ window.location.replace("");
+
+}
+function rhthnd(){
+ document.getElementById("badluck").innerHTML="You Lose!";
+ window.location.replace("");
+}
+
+
+  
